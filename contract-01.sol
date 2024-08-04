3be2b37b0a19e1159ff2125f636da2d51f6e8c8d
Chema Polo <chema@informaticapolo.com>
2024-08-04 19:31:18 +0200

    adecuación storageX

--- contract-01.sol
+++ contract-01.sol
@@ -1,29 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
-
-/**
- * @title StorageX
- * @dev Store & return variable x 5
- */
-
-contract StorageX {
-
-    uint number;
-
-    /**
-     * @dev Store value in variable
-     * @param num value to store
-     */
-    function set(uint256 num) public {
-        number = num;
-    }
-
-    /**
-     * @dev Return value *5
-     * @return value of 'number'
-     */
-    function get() public view returns (uint256){
-        return number*5;
-    }
-
-}
+	pragma solidity >=0.8.0 <0.9.0;
+	
+	/**
+	 * @title StorageX
+	 * @dev This contract allows storing a value and retrieving it multiplied by 5.
+	 */
+	contract StorageX {
+	
+	    uint256 number;
+	
+	    /**
+	     * @dev Store value in variable
+	     * @param num value to store
+	     */
+	    function set(uint256 num) public {
+	        number = num;
+	    }
+	
+	    /**
+	     * @dev Return value multiplied by 5
+	     * @return value of 'number'
+	     */
+	    function get() public view returns (uint256) {
+	        // Solidity 0.8.x automatically checks for overflow.
+	        uint256 result = number * 5;
+	
+	        // Additional check to ensure no overflow occurred.
+	        require(result / 5 == number, "Overflow occurred");
+	
+	        return result;
+	    }
+	}
